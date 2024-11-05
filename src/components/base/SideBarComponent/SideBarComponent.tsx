a0fe9d61bcfa8ed0add3a8f27462a319b14b814d
ArthurBlasi <rthrblasi@gmail.com>
2024-11-05 15:08:01 -0300

    adicionando icones e ajustando a nova sidebar

--- src/components/base/SideBarComponent/SideBarComponent.tsx
+++ src/components/base/SideBarComponent/SideBarComponent.tsx
@@ -1,22 +1,45 @@
 import './SideBarComponent.css'
-import { Flex } from "@chakra-ui/react";
-import { HamburgerIcon } from '@chakra-ui/icons';
+import { useState } from 'react';
+import { Flex, Text } from "@chakra-ui/react";
+import { RiHome6Line } from "react-icons/ri";
+import { RiCalendarLine } from "react-icons/ri";
+import { FiMenu } from "react-icons/fi";
+import { RiLogoutBoxLine } from "react-icons/ri";
 
 function SideBar(){
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleSidebar = () => setIsOpen(!isOpen);
+
     return (
-        <Flex className="sidebar">
-            <Flex className="logo">
-               <HamburgerIcon boxSize={6} color="white" marginRight={2}/>
-            </Flex>
+        <Flex className="sidebar"
+              width={isOpen ? "11vw" : "5vw"}
+              transition="width 0.3s">
+            
+            <button className="toggle-button" onClick={toggleSidebar}>
+                <FiMenu className="menu-icon" />
+                
+            </button>
+
             <Flex className="buttons">
                 <button className="home">
-                    <Flex align="center">
-                    
-                        <text className="hometext">Home</text>
+                    <Flex className="home-flex" align="center">
+                        <RiHome6Line className='house-icon'/>
+                        {isOpen && <Text ml={2} className="hometext">Home</Text>}
                     </Flex>
                 </button>
                 <button className="faq">
-                    <text className="faqtext">FAQ</text>
+                    <Flex className="faq-flex" align="center">
+                        <RiCalendarLine className='faq-icon'/>
+                        {isOpen && <Text ml={2} className="faqtext">FAQ</Text>}
+                    </Flex>
+                </button>
+                <button className="logout">
+                    <Flex className="logout-flex">
+                        <RiLogoutBoxLine className='logout-icon'/>
+                        {isOpen && <Text ml={2} className="logouttext">Sair</Text>}
+                    </Flex>
                 </button>
             </Flex>
         </Flex>
