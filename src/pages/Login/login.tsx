c8d911afbceda09b3865949da9321ef3a8be9d0a
Samuel <samuel.bitdinger@gmail.com>
2024-11-05 16:35:26 -0300

    responsividade na tela de login

--- src/pages/Login/login.tsx
+++ src/pages/Login/login.tsx
@@ -8,28 +8,26 @@ import { useNavigate } from "react-router-dom";
 function Login(){
     const navigate= useNavigate();
     return (
-      <Box bgGradient= "to-bl" gradientFrom= {"var(--gradient1)"} gradientTo= {"var(--gradient2)"} height={"100vh"} paddingTop={"114px"}>
-        <Flex justifyContent={"center"} flexDirection={"column"} alignItems={"center"}  gap={"30px"} >
-          <Flex justifyContent={"center"} flexDirection={"column"} alignItems={"center"}  gap={"30px"} backgroundColor={"#223547"} padding={"20px 30px"} borderRadius={"20px"}>
-            <Image src={Titulo} alt="Titulo" marginTop={"30px"}/>
+        <Flex bgGradient="to-bl" gradientFrom= {"var(--gradient1)"} gradientTo= {"var(--gradient2)"} height={"100vh"} width={"100vw"} justifyContent={"center"} flexDirection={"column"} alignItems={"center"}  gap={"30px"}>
+          <Flex justifyContent={"center"} flexDirection={"column"} alignItems={"center"}  gap={{base:"15px",sm:"30px"}} backgroundColor={"#223547"} padding={"20px 30px"} borderRadius={"20px"}> 
+            <Image src={Titulo} alt="Titulo"marginTop={{base:"10px",sm:"20px"}} width={{base:"100px",sm:"130px" ,md:"180px"}}/>
             <Box>
-        <Field label="Email" color={"white"}>
-          <Input placeholder="me@example.com" width={"350px"} borderRadius={"40px"} backgroundColor={"#D9D9D9"} color={"#000000"}/>
-        </Field>
+              <Field label="Email" color={"white"}>
+                <Input placeholder="me@example.com" width={{base:"200px",sm:"250px" ,md:"350px"}} borderRadius={"40px"} backgroundColor={"#D9D9D9"} color={"#000000"} fontSize={{base:"10px",sm:"12px", md:"16px"}}/>
+              </Field>
 
-        <Field label="Senha" color={"white"} marginTop={"10px"}>
-          <Input placeholder="Digite sua senha" type="password" width={"350px"} borderRadius={"40px"} backgroundColor={"#D9D9D9"} color={"#000000"}/>
-        </Field>
+              <Field label="Senha" color={"white"} marginTop={"10px"}>
+                <Input placeholder="Digite sua senha" type="password" width={{base:"200px",sm:"250px" ,md:"350px"}} borderRadius={"40px"} backgroundColor={"#D9D9D9"} color={"#000000"}/>
+              </Field>
             </Box>
-            <Button onClick= {() =>{navigate("/home")}} background={"#2E23A7"} width={"350px"} height= {"50px"} borderRadius={"20px"} fontSize={24} color={"white"} marginBottom={"-10px"} margin={"10px"}>
+            <Button onClick= {() =>{navigate("/home")}} background={"#2E23A7"} width={{base:"100px",sm:"250px" ,md:"350px"}} height= {"50px"} borderRadius={"20px"} fontSize={{sm:"18px",md:"24px"}} color={"white"} marginBottom={"-10px"} margin={"10px"}>
                 Entrar
             </Button>
             <Link to={"forgotPassword"} style={{ color: "gray", textDecoration: "underline" }}>
                 Esqueci minha senha
             </Link>
-            </Flex>
+          </Flex>
         </Flex>
-        </Box>
       )
 }
 
