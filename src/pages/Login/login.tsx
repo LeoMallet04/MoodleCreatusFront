b83fb1e204f4e3585e72a09e28fc6dd1f300e1ac
ThiagoCmrg <thiagosantoscamargo@hotmail.com>
2024-11-05 16:06:08 -0300

    Tela de Login e cores disponibilizadas.

--- src/pages/Login/login.tsx
+++ src/pages/Login/login.tsx
@@ -1,30 +1,31 @@
-import { Box, Flex, Input, Text } from "@chakra-ui/react"
+import { Box, Flex, Image, Input } from "@chakra-ui/react"
 import { Field } from "@/components/ui/field"
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
-import Logo from "../../assets/Logo.png"
+import Titulo from "../../assets/GRUPO DE ESTUDOS.svg";
+import { useNavigate } from "react-router-dom";
 
 function Login(){
+    const navigate= useNavigate();
     return (
-      <Box background={"#e4e4e7"} height={"100vh"} paddingTop={"114px"}>
+      <Box bgGradient= "to-bl" gradientFrom= {"var(--gradient1)"} gradientTo= {"var(--gradient2)"} height={"100vh"} paddingTop={"114px"}>
         <Flex justifyContent={"center"} flexDirection={"column"} alignItems={"center"}  gap={"30px"} >
-          <Flex justifyContent={"center"} flexDirection={"column"} alignItems={"center"}  gap={"30px"} backgroundColor={"#fafafa"} padding={"20px 30px"} borderRadius={"20px"}>
-            <img src={Logo} alt="Logo"/>
-            <Text style={{color: "black", fontSize: "24px", fontWeight: "bold", marginTop: "-20px"}}>Grupo de Estudos</Text>
+          <Flex justifyContent={"center"} flexDirection={"column"} alignItems={"center"}  gap={"30px"} backgroundColor={"#223547"} padding={"20px 30px"} borderRadius={"20px"}>
+            <Image src={Titulo} alt="Titulo" marginTop={"30px"}/>
             <Box>
-        <Field label="Email" color={"black"}>
-          <Input placeholder="me@example.com" width={"350px"} borderRadius={"40px"}/>
+        <Field label="Email" color={"white"}>
+          <Input placeholder="me@example.com" width={"350px"} borderRadius={"40px"} backgroundColor={"#D9D9D9"} color={"#000000"}/>
         </Field>
 
-        <Field label="Senha" color={"black"} marginTop={"10px"}>
-          <Input placeholder="Digite sua senha" type="password" width={"350px"} borderRadius={"40px"} />
+        <Field label="Senha" color={"white"} marginTop={"10px"}>
+          <Input placeholder="Digite sua senha" type="password" width={"350px"} borderRadius={"40px"} backgroundColor={"#D9D9D9"} color={"#000000"}/>
         </Field>
             </Box>
-            <Button background={"#2E23A7"} width={"350px"} height= {"50px"} borderRadius={"45px"} fontSize={24} color={"white"} marginBottom={"-10px"}>
+            <Button onClick= {() =>{navigate("/home")}} background={"#2E23A7"} width={"350px"} height= {"50px"} borderRadius={"20px"} fontSize={24} color={"white"} marginBottom={"-10px"} margin={"10px"}>
                 Entrar
             </Button>
-            <Link to={"forgotPassword"} style={{ color: "black", textDecoration: "underline" }}>
-                Esqueceu a senha?
+            <Link to={"forgotPassword"} style={{ color: "gray", textDecoration: "underline" }}>
+                Esqueci minha senha
             </Link>
             </Flex>
         </Flex>
