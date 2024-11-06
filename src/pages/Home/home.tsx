1b1c416e188e7cc942093ec61b23e4e3b76de028
K0W4 <kowaleskigabriel@gmail.com>
2024-11-06 17:21:52 -0300

    Criando componentes base

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -1,38 +1,28 @@
 import NavBar from "@/components/base/NavBar/NavBar";
 import Card from "../../components/base/Card/Card"
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
 import SideBar from "@/components/base/SideBarComponent/SideBarComponent";
 
 
 function Home(){
     return(
-        <>
-       
-        
-        <Flex  >
-        <SideBar/>
+        <Box>    
+            <Flex >
+                <SideBar/>  
+                <NavBar/>
+            </Flex>
+            <Box display={"flex-colum"} marginTop={"-500px"} marginLeft={"100px"}>
+                <Text color={"black"}>Home</Text>
 
-        
-          
-        <NavBar/>
-       
-        
-
-          
-           
-           
-        </Flex>
-        <Box display={"flex"} marginTop={"-500px"} marginLeft={"100px"}>
-                 <Card index={1} url={""} image={""}/>
-                <Card index={2} url={""} image={""}/>
-                <Card index={3} url={""} image={""}/>
-                <Card index={4} url={""} image={""}/>
-                <Card index={5} url={""} image={""}/>
+                <Box display={"flex"} my={"10"}>
+                    <Card index={1} url={""} image={""}/>
+                    <Card index={2} url={""} image={""}/>
+                    <Card index={3} url={""} image={""}/>
+                    <Card index={4} url={""} image={""}/>
+                    <Card index={5} url={""} image={""}/>
+                </Box>
+            </Box>
         </Box>
-        
-     
-         
-        </>
     )
 }
 
