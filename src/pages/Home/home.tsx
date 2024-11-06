4df76548c5e607990cf058a9322c14cf63e05561
Gustavo <melleugustavo@gmail.com>
2024-11-06 17:11:52 -0300

    home-tudo no lugar

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -1,25 +1,36 @@
 import NavBar from "@/components/base/NavBar/NavBar";
 import Card from "../../components/base/Card/Card"
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import SideBar from "@/components/base/SideBarComponent/SideBarComponent";
 
 
 function Home(){
     return(
         <>
+       
+        
         <Flex  >
-            <SideBar/>
+        <SideBar/>
 
-            <NavBar/>
-            
+        
+          
+        <NavBar/>
+       
+        
+
+          
+           
+           
         </Flex>
-            <Flex margin={"200px"} >
-                <Card index={1} url={""} image={""}/>
+        <Box display={"flex"} marginTop={"-500px"} marginLeft={"100px"}>
+                 <Card index={1} url={""} image={""}/>
                 <Card index={2} url={""} image={""}/>
                 <Card index={3} url={""} image={""}/>
                 <Card index={4} url={""} image={""}/>
                 <Card index={5} url={""} image={""}/>
-             </Flex>
+        </Box>
+        
+     
          
         </>
     )
