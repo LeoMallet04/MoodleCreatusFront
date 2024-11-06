be008b29d173623595511540824716c6bc8a80b9
alice <alicekoepp@hotmail.com>
2024-11-06 15:59:39 -0300

    Iniciando a ProfilePage

--- src/pages/Profile/profile.tsx
+++ src/pages/Profile/profile.tsx
@@ -1,9 +1,17 @@
+import { Flex } from "@chakra-ui/react";
+import NavBar from "@/components/base/NavBar/NavBar";
+import SideBar from "@/components/base/SideBarComponent/SideBarComponent";
 
-
-function Profile(){
-    return(
-        <h1>Profile</h1>
-    )
+function Profile() {
+    return (
+        <Flex direction="column" height="100vh">
+            <NavBar />
+            <Flex flex="1">
+                <SideBar />
+                {/* Aqui você pode adicionar o conteúdo principal do perfil */}
+            </Flex>
+        </Flex>
+    );
 }
 
 export default Profile;
