1c6baa62ba0e46b262729774c3d069ac2a7eea30
ArthurBlasi <rthrblasi@gmail.com>
2024-11-08 16:20:19 -0300

    responsividade provavelmente pronta

--- src/pages/Profile/components/DadosComponent.tsx
+++ src/pages/Profile/components/DadosComponent.tsx
@@ -11,28 +11,28 @@ interface DadosComponentProps {
 function DadosComponent({ curso, email, github, faltas }: DadosComponentProps) {
     return (
         <Flex
-            mt={{ base: "30px", md: "40px", lg: "50px" }}
-            ml={{ base: "20px", md: "30px", lg: "50px" }}
-            width={{ base: "80vw", md: "50vw", lg: "22vw" }}
-            height={{ base: "auto", md: "50vh", lg: "45vh" }}
+            mt={{ base: "30px", md: "40px", lg: "50px", xl: "50px" }}
+            ml={{ base: "20px", md: "30px", lg: "40px", xl: "50px" }}
+            width={{ base: "80vw", md: "60vw", lg: "40vw", xl: "22vw" }}
+            height={{ base: "auto", md: "auto", lg: "60vh", xl: "45vh" }}
             bg={"rgba(255, 255, 255, 0.12)"}
             borderRadius={"10px"}
             display={"flex"}
             flexDirection={"column"}
             zIndex={1}
             padding={"1.2rem"}
-            gap={{ base: "1rem", md: "1.5rem", lg: "2rem" }}
+            gap={{ base: "1rem", md: "1.5rem", lg: "1.5rem", xl: "2rem" }}
         >
             <Flex
                 flexDirection={"row"}
                 zIndex={10}
-                gap={{ base: "2rem", md: "3rem", lg: "4rem" }}
+                gap={{ base: "2rem", md: "2.5rem", lg: "3rem", xl: "4rem" }}
                 fontFamily={"Inter"}
             >
                 <Flex
                     flexDirection={"column"}
                     color={"#559DE6"}
-                    fontSize={{ base: "10px", md: "11px", lg: "12px" }}
+                    fontSize={{ base: "10px", md: "11px", lg: "12px", xl: "12px" }}
                     fontFamily={"Inter"}
                     width={"30%"}
                     height={"40%"}
@@ -46,7 +46,7 @@ function DadosComponent({ curso, email, github, faltas }: DadosComponentProps) {
                 <Flex
                     flexDirection={"column"}
                     color={"white"}
-                    fontSize={{ base: "10px", md: "11px", lg: "12px" }}
+                    fontSize={{ base: "10px", md: "11px", lg: "12px", xl: "12px" }}
                     fontFamily={"Inter"}
                     width={"70%"}
                     height={"40%"}
@@ -66,22 +66,22 @@ function DadosComponent({ curso, email, github, faltas }: DadosComponentProps) {
                 alignItems={"center"}
                 gap={"0.5rem"}
             >
-                <Text color={"white"} fontSize={{ base: "10px", md: "11px", lg: "12px" }}>Faltas</Text>
-                <Text color={"#559DE6"} fontSize={{ base: "26px", md: "28px", lg: "30px" }}>{faltas}</Text>
+                <Text color={"white"} fontSize={{ base: "10px", md: "11px", lg: "12px", xl: "12px" }}>Faltas</Text>
+                <Text color={"#559DE6"} fontSize={{ base: "26px", md: "28px", lg: "30px", xl: "30px" }}>{faltas}</Text>
             </Flex>
 
             <Flex alignContent={"center"} alignItems={"center"}>
                 <Button
                     width={"100%"}
-                    height={{ base: "4vh", md: "5vh", lg: "5vh" }}
+                    height={{ base: "4vh", md: "5vh", lg: "5vh", xl: "5vh" }}
                     bg={"#21262D"}
                     cursor={"pointer"}
-                    gap={{ base: "7rem", md: "8rem", lg: "9rem" }}
+                    gap={{ base: "7rem", md: "8rem", lg: "8rem", xl: "9rem" }}
                 >
-                    <Text color={"#559DE6"} fontSize={{ base: "10px", md: "11px", lg: "12px" }} fontFamily={"Inter"}>
+                    <Text color={"#559DE6"} fontSize={{ base: "10px", md: "11px", lg: "12px", xl: "12px" }} fontFamily={"Inter"}>
                         Trocar senha
                     </Text>
-                    <Icon fontSize={{ base: "12px", md: "13px", lg: "14px" }}>
+                    <Icon fontSize={{ base: "12px", md: "13px", lg: "14px", xl: "14px" }}>
                         <ImShare color={"#559DE6"} />
                     </Icon>
                 </Button>
