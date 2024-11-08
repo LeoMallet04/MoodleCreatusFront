9a56e1e21418caba50088b59758d28a1fd3b0229
ArthurBlasi <rthrblasi@gmail.com>
2024-11-08 14:50:24 -0300

    finalizando o componente de dados

--- src/pages/Profile/components/DadosComponent.tsx
+++ src/pages/Profile/components/DadosComponent.tsx
@@ -1,4 +1,5 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Text, Button, Icon } from "@chakra-ui/react";
+import { ImShare } from "react-icons/im";
 
 function DadosComponent(){
     return(
@@ -7,24 +8,30 @@ function DadosComponent(){
             ml={"50px"}
             width={"22vw"}
             height={"45vh"}
-            bg={"#FFFFFF"}
-            opacity={"12%"}
+            bg={"rgba(255, 255, 255, 0.12)"} // Usando rgba para aplicar opacidade ao fundo
             borderRadius={"10px"}
             display={"flex"}
             flexDirection={"column"}
             zIndex={1}
+            padding={"1.2rem"}
+            gap={"2rem"}
         >
             <Flex
                 display={"flex"}
                 flexDirection={"row"}
                 zIndex={10}
+                gap={"4rem"}
+                fontFamily={"Inter"}
             >
                 <Flex
                     display={"flex"}
                     flexDirection={"column"}  
-                    color={"#559DE6"} 
+                    color={"#559DE6"}
+                    fontSize={"12px"}
+                    fontFamily={"Inter"} 
                     width={"30%"}
-                    height={"40%"} 
+                    height={"40%"}
+                    gap={"0.5rem"} 
                 >
                     <Text>Curso</Text>
                     <Text>Email</Text>
@@ -35,33 +42,48 @@ function DadosComponent(){
                     display={"flex"}
                     flexDirection={"column"}
                     color={"white"} 
+                    fontSize={"12px"}
+                    fontFamily={"Inter"}
                     width={"70%"}
-                    height={"40%"}   
+                    height={"40%"}
+                    gap={"0.5rem"}   
                 >
                     <Text>Engenharia de Software</Text>
                     <Text>exemplo@gmail.com</Text>
                     <Text>Username</Text>
                 </Flex>
-
             </Flex>
 
             <Flex
                 display={"flex"}
                 flexDirection={"column"}
-                color={"white"}
                 height={"30%"}
                 zIndex={10}
+                fontFamily={"Inter"}
+                alignItems={"center"}
+                gap={"0.5rem"}
             >
-                <Text color={"white"}>Faltas</Text>
-                <Text color={"#559DE6"}>3</Text>
+                <Text color={"white"} fontSize={"12px"}>Faltas</Text>
+                <Text color={"#559DE6"} fontSize={"30px"}>3</Text>
             </Flex>
 
-            <Flex>
-                
+            <Flex
+                alignContent={"center"}
+                alignItems={"center"}
+            >
+                <Button
+                    width={"100%"}
+                    height={"5vh"}
+                    bg={"#21262D"}
+                    cursor={"pointer"}
+                    gap={"9rem"}
+                >
+                    <Text color={"#559DE6"} fontSize={"12px"} fontFamily={"Inter"}>Trocar senha</Text>
+                    <Icon fontSize={"14px"}>
+                    <ImShare color={"#559DE6"}/>
+                    </Icon>
+                </Button>
             </Flex>
-
-
-
         </Flex>
     )
 }
