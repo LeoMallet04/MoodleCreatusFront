5bf5a791628f20d2ce0a92ded364485c1edb033e
ArthurBlasi <rthrblasi@gmail.com>
2024-11-08 16:21:18 -0300

    arrumando as rotas pro pr

--- src/pages/Profile/profile.tsx
+++ src/pages/Profile/profile.tsx
@@ -1,19 +1,8 @@
-import { Flex } from "@chakra-ui/react";
-import DadosComponent from "./components/DadosComponent";
 
 
 function Profile(){
     return(
-        <Flex>
-<DadosComponent 
-    curso="Engenharia de Software" 
-    email="exemplo@gmail.com" 
-    github="Username" 
-    faltas={3} 
-/>
-
-        </Flex>
-        
+        <h1>Profile</h1>
     )
 }
 
