488ad2964a7874d926799e66db45f781991a2178
Leonardo <leonardoms.2010@hotmail.com>
2024-11-12 11:48:05 -0300

    Integração da API do GitHUb com o Front e organização de componentes da profile

--- src/pages/Profile/profile.tsx
+++ src/pages/Profile/profile.tsx
@@ -1,15 +1,11 @@
 import { useState } from "react";
-import { Flex, HStack, Stack, Text, Grid, GridItem, Badge, Box,} from "@chakra-ui/react";
+import { Flex, HStack, Stack, Text, Grid, GridItem} from "@chakra-ui/react";
 import { Avatar } from "@/components/ui/avatar";
 import SideBar from "@/components/base/SideBarComponent/SideBarComponent";
 import NavBar from "@/components/base/NavBar/NavBar";
 import DadosComponent from "./components/DadosComponent";
-import {
-    AccordionItem,
-    AccordionItemTrigger,
-    AccordionRoot,
-} from "@/components/ui/accordion";
-import AddProjeto from "./components/AddProjeto";
+import ProjetoList from "./components/ProjetoList";
+
 
 interface User {
     id: string;
@@ -18,6 +14,7 @@ interface User {
     avatar: string;
 }
 
+
 const authenticatedUser: User = {
     id: "1",
     name: "Nome Usuario",
@@ -25,11 +22,7 @@ const authenticatedUser: User = {
     avatar: "",
 };
 
-const projects = [
-    { title: "Calculadora", description: "Breve descrição do projeto de Calculadora", tags: ["CSS", "HTML", "JavaScript"] },
-    { title: "Conversor de Moedas", description: "Breve descrição do projeto de Conversor de Moedas", tags: ["React", "API", "JavaScript"] },
-    { title: "Portfolio", description: "Breve descrição do projeto de Portfolio", tags: ["React", "Chakra UI", "TypeScript"] },
-];
+
 
 const Profile: React.FC = () => {
     const [showCards, setShowCards] = useState(false);
@@ -88,73 +81,15 @@ const Profile: React.FC = () => {
                                     github="Username"
                                     faltas={0}
                                 ></DadosComponent>
+                               
                             </Stack>
                         </HStack>
 
                     </GridItem>
 
+                    <ProjetoList handleAccordionClick={handleAccordionClick} showCards={showCards}></ProjetoList>
 
-                    <GridItem marginBottom={"50%"}>
-                        <Text fontFamily="Inter" fontWeight="bold" top={"100"} fontSize={{ base: "1.25rem", md: "1.5rem" }} color="#FFFFFF">
-                            Meus Projetos
-                        </Text>
-                        <AccordionRoot collapsible defaultValue={["a"]}>
-                            <AccordionItem
-                                value="a"
-                                width={{ base: "100%", md: "50%" }}
-                                height={"30px"}
-                                border={"0rem solid rgba(255, 255, 255, 0.12)"}
-                                borderRadius={"1rem"}
-                                mb={"4"}
-                                p={"2"}
-                                bg={"rgba(255, 255, 255, 0.05)"}
-                            >
-                                <AccordionItemTrigger onClick={handleAccordionClick}>
-
-                                </AccordionItemTrigger>
-                            </AccordionItem>
-                        </AccordionRoot>
-
-                        {showCards && (
-                            <Grid
-                                templateColumns={{ base: "1fr", sm: "repeat(1fr)", lg: "repeat(1fr)" }}
-                                gap={6}
-                                mt={4}
-                                height={"0px"}
-                                width={"50%"}
-                            >
-                                {projects.map((project, index) => (
-                                    <Box
-                                        key={index}
-                                        pl={4}
-                                        pt={3}
-                                        h={"120px"}
-                                        borderRadius="lg"
-                                        bg="rgba(255, 255, 255, 0.05)"
-                                        borderColor="rgba(255, 255, 255, 0.12)"
-                                    >
-                                        <Text fontFamily="Inter" fontWeight="bold" fontSize="xl" color="#4175A6" mb={2}>
-                                            {project.title}
-                                        </Text>
-                                        <Text fontFamily="Inter" fontSize="small" color="#C2CFD9" mb={4}>
-                                            {project.description}
-                                        </Text>
-                                        <HStack>
-                                            {project.tags.map((tag, i) => (
-                                                <Badge key={i} colorScheme="purple">
-                                                    {tag}
-                                                </Badge>
-                                            ))}
-                                        </HStack>
-                                    </Box>
-                                ))}
-                            </Grid>
-                        )}
-                    </GridItem>
                 </Grid>
-                {/* <Grid
-                   
-                > */}
                    
             </Flex>
         </Flex>
