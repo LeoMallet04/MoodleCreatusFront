82625b0f2e51f2644519d8f0dd5e2a42855a0b1a
PedroKleinDavila <p.klein@edu.pucrs.br>
2024-11-12 15:12:13 -0300

    changePassword integrado totalmente com o back

--- src/pages/ChangePassword/changePassword.tsx
+++ src/pages/ChangePassword/changePassword.tsx
@@ -1,25 +1,35 @@
 import { Field } from "@/components/ui/field";
 import { Box, Button, Flex, Input, Image } from "@chakra-ui/react";
 import Titulo from "@/assets/GRUPO DE ESTUDOS.svg";
+import axios from "axios";
+import { useState } from "react";
 
 function ChangePassword() {
+  const [newPassword, setNewPassword] = useState('');
   const urlParams = new URLSearchParams(window.location.search);
   const token = urlParams.get('token');
   document.cookie = `jwt=${token}; max-age=600;path=/; secure; SameSite=Strict`;
   async function changePassword() {
-    const data=await fetch('http://localhost:3000/user/changePW', {
-      method: 'PUT',
-      credentials: 'include', 
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`
-      },
-      body: JSON.stringify({oldPassword: '12345', newPassword  : '123456'}),//to do pegar os valores dos inputs
-    })
-    if (data.ok) {
-      console.log('Senha alterada com sucesso');
-    } else {
-      console.log('Erro ao alterar a senha');
+    try {
+      const response = await axios.put(
+        'http://localhost:3000/user/changePW',
+        { newPassword },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`
+          },
+          withCredentials: true
+        }
+      );
+    
+      if (response.status === 200) {
+        console.log('Senha alterada com sucesso');
+      } else {
+        console.log('Erro ao alterar a senha');
+      }
+    } catch (error) {
+      console.log('Erro ao alterar a senha', error);
     }
   }
   return (
@@ -28,7 +38,7 @@ function ChangePassword() {
             <Image src={Titulo} alt="Titulo" marginTop={{base:"10px",sm:"20px"}} width={{base:"100px",sm:"130px" ,md:"180px"}}/>
             <Box>
               <Field label={"Nova Senha"} color={"white"}>
-                <Input placeholder={"*****"} width={{base:"200px",sm:"250px" ,md:"350px"}} borderRadius={"40px"} backgroundColor={"#D9D9D9"} color={"#000000"} fontSize={{base:"10px",sm:"12px", md:"16px"}}/>
+                <Input placeholder={"*****"} value={newPassword} onChange={(e) => setNewPassword(e.target.value)} width={{base:"200px",sm:"250px" ,md:"350px"}} borderRadius={"40px"} backgroundColor={"#D9D9D9"} color={"#000000"} fontSize={{base:"10px",sm:"12px", md:"16px"}}/>
               </Field>
               <Field label={"Confirmar Senha"} color={"white"} marginTop={"10px"}>
                 <Input placeholder={"*****"} type="password" width={{base:"200px",sm:"250px" ,md:"350px"}} borderRadius={"40px"} backgroundColor={"#D9D9D9"} color={"#000000"}/>
@@ -42,4 +52,4 @@ function ChangePassword() {
   )
 }
 
-export default ChangePassword;
+export default ChangePassword;
