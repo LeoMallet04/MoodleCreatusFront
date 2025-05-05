b6dd33fa26a55e7781849c6af36d2eea8717f128
LeoMallet04 <leonardoms.2010@hotmail.com>
2025-05-05 15:01:54 -0300

    refact: Remoção da requisição dos repos pela API do GitHub do front para o back e configuração do novo endpoint do backend

--- src/utils/apiGit.ts
+++ src/utils/apiGit.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import getGitToken, { GitToken } from "./gitToken";
 
 const BASE_URL = "https://api.github.com/repos";
 const URL_COLORS = "https://raw.githubusercontent.com/ozh/github-colors/master/colors.json";
@@ -19,58 +18,30 @@ async function getReposData(user_name: string, repo_name: string): Promise<GitDT
   console.log("URL da API do GitHub:", url);
 
   try {
-    const gitToken: GitToken | undefined = await getGitToken();
-
-    if(!gitToken || gitToken == undefined){
-      throw new Error("Token inválido");
-    }
-
-    console.log(gitToken.token);
-
-    const response = await axios.get(url, {
-      headers: {
-        Authorization: `token ${gitToken.token}`,
-        Accept: `application/vnd.github+json`,
-        'X-GitHub-Api-Version': '2022-11-28',
-      }
-    });
     
-    console.log("Authorization Header:", response.headers['authorization'])
+    const response = await axios.post("http://localhost:3000/git-api/repos-auth", {
+    url
+    },{
+      withCredentials: true
+    });
 
-    const json_data = await response.data;
+    const data = response.data;
 
-    const repo_name = json_data.name ?? "";
-    const repo_description = json_data.description ?? "";
-    //Aqui chamamos a função getLanguagesData() passando a URL das linguagens que contêm na response
-    const repo_languages = await getLanguageData(json_data.languages_url);
-    const repo_url = json_data.html_url ?? "";
-
-
-    return {
-      repo_name,
-      repo_description,
-      repo_languages,
-      repo_url
+    const gitDto: GitDTO = {
+      repo_name: data.repo_name,
+      repo_description: data.repo_description,
+      repo_languages: data.repo_languages,
+      repo_url: data.repo_url,
     };
+
+    return gitDto;
+  
   } catch (error) {
     console.error("Erro ao buscar os dados do repositório:", error);
     return null;
   }
 }
 
-//Aqui pegamos as linguagens usadas em um repositório com base na URL das linguagens cujo está dentro
-//da response dos dados obtidos pela API do GitHub
-async function getLanguageData(languages_url: string): Promise<string[]> {
-  try {
-    const response_languages = await axios.get(languages_url);
-
-    const json_data = await response_languages.data;
-    return Object.keys(json_data);
-  } catch (error) {
-    console.error("Erro ao buscar linguagens: ", error);
-    return [];
-  }
-}
 
 async function getLanguagesColor(languages:string[]): Promise<Record<string,string> | undefined>{
   const languages_colors: Record<string, string> = {};
@@ -92,4 +63,4 @@ async function getLanguagesColor(languages:string[]): Promise<Record<string,stri
   
 }
 
-export {getLanguageData, getReposData, getLanguagesColor}
+export {getReposData, getLanguagesColor}
