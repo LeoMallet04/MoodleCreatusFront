bb3600bc71af74b842abd92a41c34e83ad36b760
LeoMallet04 <leonardoms.2010@hotmail.com>
2025-05-05 12:21:09 -0300

    feat: Criação de uma utils responsável por pegar o gitToken do backend para validar as requisições

---
+++ src/utils/gitToken.ts
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+export interface GitToken {
+    token: string; // O token de acesso
+    expires_at: string; // Data de expiração do token (ISO 8601)
+    permissions?: Record<string, string>; // Permissões concedidas ao token (opcional)
+    repository_selection?: string;
+}
+
+//Pegamos o token do GitHub APP pelo endpoint do BackEnd
+async function getGitToken(): Promise<GitToken | undefined>{
+    try {
+        const response = await axios.post('http://localhost:3000/git-token/access-personal-token',
+            {},
+            {
+                withCredentials: true
+            }
+        );
+
+        console.log(response.data);
+
+      
+        const data = response.data;
+
+        console.log(data.token);    
+
+
+        const gitToken: GitToken = {
+            token: data.token,
+            expires_at: data.expires_at,
+            permissions: data.permissions,
+            repository_selection: data.repository_selection
+        }
+
+        return gitToken;
+
+    } catch (error) {
+        console.log("Erro ao fazer a requisição do token", error)
+    }
+
+}
+
+export default getGitToken
+
