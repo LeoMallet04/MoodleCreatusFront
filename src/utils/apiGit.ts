58a53272bb508d3b6d321d20e9d09591f7d44309
LeoMallet04 <leonardoms.2010@hotmail.com>
2025-05-05 12:22:59 -0300

    refact: Alterações na chamada da ApiGit usando axios agora e adição de comentários para explicar as funcionalidades

--- src/utils/apiGit.ts
+++ src/utils/apiGit.ts
@@ -1,4 +1,8 @@
+import axios from "axios";
+import getGitToken, { GitToken } from "./gitToken";
+
 const BASE_URL = "https://api.github.com/repos";
+const URL_COLORS = "https://raw.githubusercontent.com/ozh/github-colors/master/colors.json";
 
 interface GitDTO {
   repo_name: string;
@@ -7,27 +11,40 @@ interface GitDTO {
   repo_url: string;
 }
 
+
+//Nessa função pegamos os dados do repositório pela API do GitHub com base no username do GitHub
+//e o nome do repositório desejado
 async function getReposData(user_name: string, repo_name: string): Promise<GitDTO | null> {
   const url = `${BASE_URL}/${user_name}/${repo_name}`;
   console.log("URL da API do GitHub:", url);
+
   try {
-    const response = await fetch(url);
-    
-    if (!response.ok) {
-      throw new Error(`Erro na requisição: ${response.statusText}`);
+    const gitToken: GitToken | undefined = await getGitToken();
+
+    if(!gitToken || gitToken == undefined){
+      throw new Error("Token inválido");
     }
 
-    const json_data = await response.json();
+    console.log(gitToken.token);
+
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: `token ${gitToken.token}`,
+        Accept: `application/vnd.github+json`,
+        'X-GitHub-Api-Version': '2022-11-28',
+      }
+    });
+    
+    console.log("Authorization Header:", response.headers['authorization'])
+
+    const json_data = await response.data;
 
     const repo_name = json_data.name ?? "";
     const repo_description = json_data.description ?? "";
+    //Aqui chamamos a função getLanguagesData() passando a URL das linguagens que contêm na response
     const repo_languages = await getLanguageData(json_data.languages_url);
     const repo_url = json_data.html_url ?? "";
 
-    console.log(`Nome do repositório: ${repo_name}`);
-    console.log(`Descrição: ${repo_description}`);
-    console.log(`Linguagens: ${repo_languages.join(", ")}`);
-    console.log(`Url do Repositório:  ${repo_url}`);
 
     return {
       repo_name,
@@ -41,18 +58,16 @@ async function getReposData(user_name: string, repo_name: string): Promise<GitDT
   }
 }
 
+//Aqui pegamos as linguagens usadas em um repositório com base na URL das linguagens cujo está dentro
+//da response dos dados obtidos pela API do GitHub
 async function getLanguageData(languages_url: string): Promise<string[]> {
   try {
-    const response = await fetch(languages_url);
-    
-    if (!response.ok) {
-      throw new Error(`Erro na requisição: ${response.statusText}`);
-    }
+    const response_languages = await axios.get(languages_url);
 
-    const json_data = await response.json();
+    const json_data = await response_languages.data;
     return Object.keys(json_data);
   } catch (error) {
-    console.error("Erro ao buscar linguagens:", error);
+    console.error("Erro ao buscar linguagens: ", error);
     return [];
   }
 }
