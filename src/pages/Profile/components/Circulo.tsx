2c9e43a30700ad949c843c38a01c9885bc14f9e7
LeoMallet04 <leonardoms.2010@hotmail.com>
2025-05-09 15:29:40 -0300

    refact: Alterações na lógico do Circulo para não perder as cores ao mudar de aba

--- src/pages/Profile/components/Circulo.tsx
+++ src/pages/Profile/components/Circulo.tsx
@@ -1,5 +1,6 @@
 import { Box } from '@chakra-ui/react';
 import { LanguageColorsDTO } from './ProjetoList';
+import { useEffect, useState } from 'react';
 
 interface CirculoProps{
   language: string;
@@ -8,22 +9,54 @@ interface CirculoProps{
 
 
 function Circulo({language, colorsDTO}:CirculoProps) {  
-  var languageColor = "lightgray";
-  if(colorsDTO){
-    const raw = localStorage.getItem(`language_colors_${colorsDTO.projectId}`);
-    if(raw){
-      try {
-        const data: LanguageColorsDTO = JSON.parse(raw);
+  const [languageColor,setLanguageColor] = useState("lightgray");
 
-        if (data.languageColors && data.languageColors[language]) {
-          languageColor = data.languageColors[language];
-        }
-      } catch (error) {
-        console.error("Erro ao fazer parse do localStorage", error)
+  const carregarDados = () => {
+    
+
+    if(!colorsDTO) return;
+
+    //Aqui pegamos os dados que foram armazenados localmente na ProjetoList que são referentes ao LanguageColorsDTO
+    const raw = localStorage.getItem(`language_colors_${colorsDTO.projectId}`);
+    
+    if(!raw) return;
+
+
+    try {
+      //Transformamos de JSON para LanguageColorsDTO
+      const data: LanguageColorsDTO = JSON.parse(raw);
+
+      if (data.languageColors && data.languageColors[language]) {
+        //Configuramos o languageColor atual para o valor do Record 'languageColors' da chave 'language'
+        setLanguageColor(data.languageColors[language]);
+      }else{
+        console.log("Cor não encontrada para linguagem", language)
       }
+    } catch (error) {
+      console.error("Erro ao fazer parse do localStorage", error)
+    }
+    
+  }
+
+  //A PARTIR DAQUI IREMOS IMPLEMENTAR A LÓGICA PARA GUARDAR A COR ATUAL LOCALMENTE E NÂO PERDE-LA AO MUDAR DE ABA
+
+  const saveLanguageColor = () => {
+    if(languageColor !== "lightgray"){
+      localStorage.setItem(`languageColor${language}`, languageColor)
+    }else{
+      carregarDados();
     }
   }
 
+  useEffect(()=>{
+    saveLanguageColor();
+    const color = localStorage.getItem(`languageColor${language}`);
+    if(color != null){
+      setLanguageColor(color);
+    }
+
+  },[languageColor,language]) 
+  
 
   return (
     <Box
